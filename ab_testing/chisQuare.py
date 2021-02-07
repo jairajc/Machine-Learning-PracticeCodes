d5ce3bd8a33a4a2fd132d2fd5784b22b390bcd8e
Jai Raj Choudhary <jairaj.c9@gmail.com>
2021-02-07 16:23:48 +0530

    Update chisQuare.py

--- ab_testing/chisQuare.py
+++ ab_testing/chisQuare.py
@@ -1,6 +1,5 @@
 # From the course: Bayesin Machine Learning in Python: A/B Testing
-# https://deeplearningcourses.com/c/bayesian-machine-learning-in-python-ab-testing
-# https://www.udemy.com/bayesian-machine-learning-in-python-ab-testing
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import chi2, chi2_contingency
